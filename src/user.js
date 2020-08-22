e090b27330ef58ea8023bec827a50646e86ea2e3
Yatin darmwal <yatin@infeedo.com>
2020-08-22 13:53:30 +0530

    validation on length

--- src/user.js
+++ src/user.js
@@ -6,7 +6,11 @@ const schema = mongoose.Schema;
 const UserSchema = new schema({
     name: {
         type: String,
-        required: [true, 'Name is required.']
+        required: [true, 'Name is required.'],
+        validate: {
+            validator: (name) => name.length > 2,
+            message: 'Name must be longer then 2 characters.'
+        }
     },
     postCount: Number
 });
