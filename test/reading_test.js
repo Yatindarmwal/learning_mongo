e81a6df2e85c8fc824c4026af99d15fe09ba750d
Yatin darmwal <yatin@infeedo.com>
2020-09-05 20:41:12 +0530

    skip limit sort

--- test/reading_test.js
+++ test/reading_test.js
@@ -6,7 +6,10 @@ describe('Reading users out of the database', () => {
     let joe;
     beforeEach((done) => {
         joe = new User({ name: 'joe' });
-        joe.save()
+        kit = new User({ name: 'kit' });
+        alex = new User({ name: 'alex' });
+        zack = new User({ name: 'zack' });
+        Promise.all([joe.save(), kit.save(), alex.save(), zack.save()])
             .then(() => done());
     });
     it('Find all users with name joe', (done) => {
@@ -21,5 +24,15 @@ describe('Reading users out of the database', () => {
             done();
         });
     });
-
+    it('can skip and limit the result set', (done) => {
+        User.find({})
+            .sort({ name: 1 })
+            .skip(1)
+            .limit(2)
+            .then(users => {
+                assert(users[0].name == 'joe');
+                assert(users[1].name == 'kit');
+                done();
+            });
+    });
 });
